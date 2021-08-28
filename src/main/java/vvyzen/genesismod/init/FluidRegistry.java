ac9d7e380777832300eba8e37194d8b8be641956
vvyzen <89709773+vvyzen@users.noreply.github.com>
2021-08-28 16:43:11 -0500

    h

--- src/main/java/vvyzen/genesismod/init/FluidRegistry.java
+++ src/main/java/vvyzen/genesismod/init/FluidRegistry.java
@@ -14,9 +14,9 @@ public class FluidRegistry {
     public static final DeferredRegister<Fluid> FLUIDS = DeferredRegister.create(ForgeRegistries.FLUIDS, GenesisMod.MODID);
 
     //fuck
-    //public static final RegistryObject<BloodFluid.Flowing> FLOWING_BLOOD = FLUIDS.register("flowing_blood", () -> new BloodFluid.Flowing(FluidProperties.BLOOD_PROPERTIES));
-    //public static final RegistryObject<BloodFluid.Source> BLOOD_SOURCE = FLUIDS.register("blood_source", () -> new BloodFluid.Source(FluidProperties.BLOOD_PROPERTIES) );
-    public static final RegistryObject<ForgeFlowingFluid.Flowing> FLOWING_BLOOD = FLUIDS.register("flowing_blood", () -> new ForgeFlowingFluid.Flowing(FluidProperties.BLOOD_PROPERTIES));
-    public static final RegistryObject<ForgeFlowingFluid.Source> BLOOD_SOURCE = FLUIDS.register("blood_source", () -> new ForgeFlowingFluid.Source(FluidProperties.BLOOD_PROPERTIES) );
+    public static final RegistryObject<BloodFluid.Flowing> FLOWING_BLOOD = FLUIDS.register("flowing_blood", () -> new BloodFluid.Flowing(FluidProperties.BLOOD_PROPERTIES));
+    public static final RegistryObject<BloodFluid.Source> BLOOD_SOURCE = FLUIDS.register("blood_source", () -> new BloodFluid.Source(FluidProperties.BLOOD_PROPERTIES) );
+    //public static final RegistryObject<ForgeFlowingFluid.Flowing> FLOWING_BLOOD = FLUIDS.register("flowing_blood", () -> new ForgeFlowingFluid.Flowing(FluidProperties.BLOOD_PROPERTIES));
+    //public static final RegistryObject<ForgeFlowingFluid.Source> BLOOD_SOURCE = FLUIDS.register("blood_source", () -> new ForgeFlowingFluid.Source(FluidProperties.BLOOD_PROPERTIES) );
 
 }
