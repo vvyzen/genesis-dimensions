f21c8ad8194f36d6f045d14b33e9c3f128cd9d7c
vvyzen <89709773+vvyzen@users.noreply.github.com>
2021-08-29 22:16:59 -0500

    the devil is real and his name is ISkyRenderHandler

--- src/main/java/vvyzen/genesismod/client/ClientRegistrar.java
+++ src/main/java/vvyzen/genesismod/client/ClientRegistrar.java
@@ -1,15 +1,18 @@
 package vvyzen.genesismod.client;
 
+import net.minecraft.client.renderer.DimensionSpecialEffects;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import vvyzen.genesismod.client.renderer.OtherworldRenderInfo;
 
 @OnlyIn(Dist.CLIENT)
 @Mod.EventBusSubscriber(modid = "ssvrfi", value = {Dist.CLIENT}, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ClientRegistrar {
     public static void setup(FMLCommonSetupEvent event){
 
-    //put(new ResourceLocation("ssvrfi", "otherworld"), new OtherworldRenderInfo());
+        DimensionSpecialEffects.EFFECTS.put(new ResourceLocation("ssvrfi", "otherworld"), new OtherworldRenderInfo());
     }
 }
